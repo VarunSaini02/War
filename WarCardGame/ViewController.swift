56af60d6d141def5957027a31eec8db2f406fd15
VarunSaini02 <varun.saini@gmail.com>
2020-04-09 12:12:03 -0500

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by 90304566 on 1/29/20.
+//  Copyright © 2020 Varun Saini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+    }
+    
+    // Function for when button is tapped
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftNumber = Int.random(in: 2...14)
+        
+        let rightNumber = Int.random(in: 2...14)
+        
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        if leftNumber > rightNumber {
+            // Left wins
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+            
+        } else if rightNumber > leftNumber {
+            // Right wins
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+            
+        } else {
+            // Tie
+        }
+        
+    }
+    
+}
+
